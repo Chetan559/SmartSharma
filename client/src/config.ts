12efc64ae0fb95b2a561227c6cc79e43c61981b2
Chetan Sharma <cschetan559@gmail.com>
2026-04-20 21:49:12 +0530

    bug fix

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,3 @@
-export const BACKEND_URL = "https://smartsharma.onrender.com";
+// export const BACKEND_URL = "https://smartsharma.onrender.com";
 // export const BACKEND_URL = "http://localhost:5000";
+export const BACKEND_URL = "https://smart-sharma-server.vercel.app/";
